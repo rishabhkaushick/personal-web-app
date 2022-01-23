731a4b1bfffbe1a3e46296cd3682710424f32e94
Rishabh Kaushick <rishabh.kaushick@gmail.com>
2022-01-23 18:10:30 +0530

    Added side navigation functionality in html, css and js files

---
+++ js/scriptfile.js
@@ -0,0 +1,9 @@
+function openSideNav(){
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("hamburger").style.display = "none";
+}
+
+function closeSideNav(){
+    document.getElementById("mySideNav").style.width = "0";
+    document.getElementById("hamburger").style.display = "block";
+}
